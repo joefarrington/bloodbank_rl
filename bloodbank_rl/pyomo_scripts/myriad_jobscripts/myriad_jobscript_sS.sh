304c00a5f4564d54411e965e944eb59248a8e382
Joe Farrington <farrington.joe@gmail.com>
2021-09-26 12:14:50 +0100

    New directory of Myriad jobscripts for each Pyomo model

---
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/myriad_jobscript_sS.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -l
+
+#$ -N pyomo_stochastic_sS
+#$ -l h_rt=24:00:00
+#$ -l mem=4G
+#$ -pe smp 8
+#$ -cwd
+
+conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
+module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
+python3 run_pyomo_smilp.py model_constructor=sS_PyomoModelConstructor n_scenarios=20 +solver_options.milpgap=0.01
