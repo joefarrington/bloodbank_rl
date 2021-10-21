5aa1e0f92d543f79a7d24dd08b3f8d6449bc7281
Joe Farrington <farrington.joe@gmail.com>
2021-10-21 20:36:07 +0100

    Update Myriad jobscripts to group into folders and add set to run without the additional FIFO constraints to show that opt is quicker

---
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/non_weekly_without_additional_fifo/myriad_jobscript_sSbQ.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -l
+
+#$ -N pyomo_stochastic_sSbQ
+#$ -l h_rt=38:00:00
+#$ -l mem=4G
+#$ -pe smp 8
+#$ -cwd
+
+conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
+module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
+python3 run_pyomo_smilp.py model_constructor=sSbQ_PyomoModelConstructor n_scenarios=20 +solver_options.TimeLimit=129600 model_constructor_params.weekly_policy=False model_constructor_params.additional_fifo_constraints=False log_folder_name=sSbQ_non_weekly_without_additional_fifo
---
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/weekly_without_additional_fifo/myriad_jobscript_sQ_weekly.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -l
+
+#$ -N pyomo_stochastic_sQ
+#$ -l h_rt=38:00:00
+#$ -l mem=4G
+#$ -pe smp 8
+#$ -cwd
+
+conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
+module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
+python3 run_pyomo_smilp.py model_constructor=sQ_PyomoModelConstructor n_scenarios=20 +solver_options.TimeLimit=129600 model_constructor_params.weekly_policy=True log_folder_name=sQ_weekly
