3c72dbc47d882ef513ae5b42257a166401b7160c
Joe Farrington <farrington.joe@gmail.com>
2021-10-26 11:55:54 +0100

    Update Myriad jobscripts, adding set that use Gurobi parameter MIPFocus=1

--- bloodbank_rl/pyomo_scripts/myriad_jobscripts/weekly_MIPFocus1/myriad_jobscript_sSaQ_weekly.sh
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/weekly_MIPFocus1/myriad_jobscript_sSaQ_weekly.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -l
 
-#$ -N pyomo_stochastic_sSbQ
+#$ -N pyomo_stochastic_sSaQ
 #$ -l h_rt=38:00:00
 #$ -l mem=4G
 #$ -pe smp 8
@@ -8,4 +8,4 @@
 
 conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
 module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
-python3 run_pyomo_smilp.py model_constructor=sSbQ_PyomoModelConstructor n_scenarios=20 +solver_options.TimeLimit=129600 model_constructor_params.weekly_policy=True log_folder_name=sSbQ_weekly
+python3 run_pyomo_smilp.py model_constructor=sSaQ_PyomoModelConstructor n_scenarios=20 +solver_options.TimeLimit=129600 +solver_options.MIPFocus=1 model_constructor_params.weekly_policy=True log_folder_name=sSaQ_weeklyMIPFocus1
