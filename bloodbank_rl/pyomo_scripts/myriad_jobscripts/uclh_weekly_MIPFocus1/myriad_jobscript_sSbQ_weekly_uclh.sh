013a1291e3074c31796058dfe0959744ee8ac0fc
Joe Farrington <farrington.joe@gmail.com>
2021-10-28 13:59:05 +0100

    Create jobscripts for Pyomo models using real demand data

---
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/uclh_weekly_MIPFocus1/myriad_jobscript_sSbQ_weekly_uclh.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -l
+
+#$ -N pyomo_stochastic_sSbQ
+#$ -l h_rt=38:00:00
+#$ -l mem=4G
+#$ -pe smp 8
+#$ -cwd
+
+conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
+module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
+python3 run_pyomo_smilp.py model_constructor=sSbQ_PyomoModelConstructor n_scenarios=20 +solver_options.MIPFocus=1 +solver_options.TimeLimit=129600 model_constructor_params.weekly_policy=True model_constructor_params.initial_inventory.2=25 demand_provider=DFPyomoDemandProvider +demand_provider_kwargs=2015_2016_real_demand scenario_name_start=0 log_folder_name=sSbQ_uclh_weeklyMIPFocus1
