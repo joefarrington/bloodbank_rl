12d7284693e2e00dbfe12ba6d72b903686931a50
Joe Farrington <farrington.joe@gmail.com>
2021-10-30 10:58:11 +0100

    Set big-M to 80 for real data sQ weekly to avoid rounding errors

--- bloodbank_rl/pyomo_scripts/myriad_jobscripts/uclh_weekly_MIPFocus1/myriad_jobscript_sQ_weekly_uclh.sh
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/uclh_weekly_MIPFocus1/myriad_jobscript_sQ_weekly_uclh.sh
@@ -8,4 +8,4 @@
 
 conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
 module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
-python3 run_pyomo_smilp.py model_constructor=sQ_PyomoModelConstructor n_scenarios=20 +solver_options.TimeLimit=129600 +solver_options.MIPFocus=1 model_constructor_params.weekly_policy=True model_constructor_params.initial_inventory.2=25 demand_provider=DFPyomoDemandProvider +demand_provider_kwargs=2015_2016_real_demand scenario_name_start=0 log_folder_name=sQ_uclh_weeklyMIPFocus1
+python3 run_pyomo_smilp.py model_constructor=sQ_PyomoModelConstructor n_scenarios=20 +solver_options.TimeLimit=129600 +solver_options.MIPFocus=1 model_constructor_params.weekly_policy=True model_constructor_params.M=80 model_constructor_params.initial_inventory.2=25 demand_provider=DFPyomoDemandProvider +demand_provider_kwargs=2015_2016_real_demand scenario_name_start=0 log_folder_name=sQ_uclh_weeklyMIPFocus1
