f32ca51885c4814a1642e160d39770621df144d6
Joe Farrington <farrington.joe@gmail.com>
2022-09-22 11:07:20 +0100

    Create hydra experiment files for SMILP experiments, and new myriad jobscripts to run them

---
+++ bloodbank_rl/pyomo_scripts/myriad_jobscripts/simulated_demand_data/sQ_weekly.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -l
+
+#$ -N pyomo_stochastic_sQ
+#$ -l h_rt=38:00:00
+#$ -l mem=4G
+#$ -pe smp 8
+#$ -cwd
+
+conda activate optim && echo 'Conda environment activated' || echo 'Activating conda environment failed'
+module load gurobi/8.1.1 && echo 'Gurobi loaded' || echo 'Loading Gurobi failed'
+python3 run_pyomo_smilp.py +experiment=simulated_demand_data/sQ_weekly
